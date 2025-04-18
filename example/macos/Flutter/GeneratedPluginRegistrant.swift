48261a18b241ec2f117f16363f82cc99f284bc08
Edson-G <gonzalezedson.junior@gmail.com>
2025-04-17 22:01:19 -0300

    Include Native SDK as module

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_live2d_plugin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLive2dPlugin.register(with: registry.registrar(forPlugin: "FlutterLive2dPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_live2d_plugin/flutter_live2d_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterLive2dPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLive2dPluginCApi"));
+}
